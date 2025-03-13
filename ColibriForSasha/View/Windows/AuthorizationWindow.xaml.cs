d55dd7524358483ebfaf01f89f87ed94ec31c733
Анастасия Мусатова <musatik2016@ya.ru>
2025-03-13 23:11:15 +0300

    Добавьте файлы проекта.

---
+++ ColibriForSasha/View/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ColibriForSasha.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfHL_Click(object sender, RoutedEventArgs e)
+        {
+
+            ConfPolicyWindow confPolicyWindow = new ConfPolicyWindow();
+            confPolicyWindow.ShowDialog();
+        }
+
+        private void RegistrationHL_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+            Close();
+        }
+
+        private void EntryBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ColibriForSasha/View/Windows/RegistrationWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ColibriForSasha.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfHL_Click(object sender, RoutedEventArgs e)
+        {
+            ConfPolicyWindow confPolicyWindow = new ConfPolicyWindow();
+            confPolicyWindow.ShowDialog();
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void EntryHL_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorizationWindow authorizationWindow = new AuthorizationWindow();
+            authorizationWindow.Show();
+            Close();
+        }
+    }
+}
